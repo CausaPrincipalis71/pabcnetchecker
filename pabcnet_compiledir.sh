8653ecc4457bfdfd1d9819051db1a049f6aa6598
Leonid Znamenok <respublica@altlinux.org>
2022-09-07 18:52:45 +0300

    Added nested search

--- pabcnet_compiledir.sh
+++ pabcnet_compiledir.sh
@@ -3,7 +3,16 @@ for directory in $(ls $1)
 do
 	for file in $(ls $1/$directory | grep -v "*.exe")
 	do
+		if [ -d $1/$directory/$file ]
+		then
+			for truefile in $(ls $1/$directory/$file | grep -v ".exe"
+			do
+				pabcnetc "$1/$directory/$file/$truefile" >> $2/$directory.txt
+				printf "\nFile $1/$directory/$file/$truefile\nExit code $? \n\n" >> $2/$directory.txt
+			done
+		else
 		pabcnetc "$1/$directory/$file" >> $2/$directory.txt
 		printf "\nFile $1$directory$file\nExit code $? \n\n" >> $2/$directory.txt
+		fi
 	done
 done
