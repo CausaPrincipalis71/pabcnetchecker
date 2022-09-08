593453948e78c5ab8b8641fd266ba3a02be0acd1
Leonid Znamenok <respublica@altlinux.org>
2022-09-08 15:46:22 +0300

    Checked all examples

--- pabcnet_compiledir.sh
+++ pabcnet_compiledir.sh
@@ -1,18 +1,33 @@
 #!/bin/bash
 for directory in $(ls $1)
 do
-	for file in $(ls $1/$directory | grep -v "*.exe")
-	do
-		if [ -d $1/$directory/$file ]
-		then
-			for truefile in $(ls $1/$directory/$file | grep -v ".exe"
-			do
-				pabcnetc "$1/$directory/$file/$truefile" >> $2/$directory.txt
-				printf "\nFile $1/$directory/$file/$truefile\nExit code $? \n\n" >> $2/$directory.txt
-			done
-		else
-		pabcnetc "$1/$directory/$file" >> $2/$directory.txt
-		printf "\nFile $1$directory$file\nExit code $? \n\n" >> $2/$directory.txt
+	if [ -d $1/$directory ]
+	then
+		for file in $(ls $1/$directory)
+		do
+			if [ -d $1/$directory/$file ]
+			then
+				for truefile in $(ls $1/$directory/$file | grep -e "*.pas")
+				do
+					pabcnetc "$1/$directory/$file/$truefile" >> $2/$directory.txt
+					printf "\nFile $1/$directory/$file/$truefile\nExit code $? \n\n" >> $2/$directory.txt
+				done
+			else
+			echo $file | grep -e ".pas"
+			if [ "$?" = "0" ]
+				then
+				pabcnetc "$1/$directory/$file" >> $2/$directory.txt
+				printf "\nFile $1$directory$file\nExit code $? \n\n" >> $2/$directory.txt
+				fi
+			fi
+		done
+	elif [ -f $1/$directory ]
+	then
+	echo $directory | grep -e ".pas"
+		if [ "$?" = "0" ]
+			then
+			pabcnetc "$1/$directory" >> $2/$directory.txt
+			printf "\nFile $1$directory\nExit code $? \n\n" >> $2/$directory.txt
 		fi
-	done
+	fi
 done
--- pabcnet_logcheck.sh
+++ pabcnet_logcheck.sh
@@ -2,7 +2,7 @@
 
 for file in $(ls $1)
 do
-	cat $1/$file | grep -e "Exit code 1" -B 1
+	cat $1/$file | grep -e "Exit code $2" -B 1
 	if [ "$?" = "0" ]
 	then
 		printf '\n'
